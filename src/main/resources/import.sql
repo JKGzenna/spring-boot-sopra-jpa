a5b6a2811ea2154f0321759f1a276572bc74e14b
JKGzenna <juanlu360@gmail.com>
2020-01-21 08:18:01 +0100

    update osh config

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -48,7 +48,6 @@ INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3, 2, 6);
 INSERT INTO `users` (username, password, enabled) VALUES ('juan','$2a$10$O9wxmH/AeyZZzIS09Wp8YOEMvFnbRVJ8B4dmAMVSGloR62lj.yqXG',1);
 INSERT INTO `users` (username, password, enabled) VALUES ('admin','$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS',1);
 INSERT INTO `users` (username, password, enabled) VALUES ('leti','$2a$10$UJ6z36fiiXhueTwq7GSkV.5t9Fn7NAWuGl0yYDbgwaKqR78pRkXH.',1);
-INSERT INTO `users` (username, password, enabled) VALUES ('cayetana','$10$JyMr7skyGhcgYBR2MvAjxuqTv7KSt2QtuCYDi4UZ5JZPsYk1OMiSi',1);
 
 INSERT INTO `authorities` (user_id, authority) VALUES (1,'ROLE_USER');
 INSERT INTO `authorities` (user_id, authority) VALUES (2,'ROLE_ADMIN');
