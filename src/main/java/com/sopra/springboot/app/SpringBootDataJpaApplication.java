0d07111ec103407ba3acf9a54873da46039c761f
JKGzenna <juanlu360@gmail.com>
2020-01-28 23:03:48 +0100

    Update SpringBootDataJpaApplication.java

--- src/main/java/com/sopra/springboot/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/sopra/springboot/app/SpringBootDataJpaApplication.java
@@ -60,7 +60,9 @@ import com.sopra.springboot.app.models.service.IUploadFileService;
         /* DESCOMENTAR ESTO SI QUEREMOS QUE NOS DESTRUYA 
 		LA CARPETA UPLOADS CADA VEZ QUE ARRANQUE Y PARE */
 //		uploadFileService.deleteAll();
-		uploadFileService.init();
+		
+		
+//		uploadFileService.init();
 		
 		
 		
