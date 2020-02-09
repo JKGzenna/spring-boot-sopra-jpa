60c0036f92f66c8817a250bc6854d58389468842
JKGzenna <juanlu360@gmail.com>
2020-02-09 05:56:26 +0100

    Update SpringBootDataJpaApplication.java

--- src/main/java/com/sopra/springboot/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/sopra/springboot/app/SpringBootDataJpaApplication.java
@@ -61,8 +61,8 @@ import com.sopra.springboot.app.models.service.IUploadFileService;
 		LA CARPETA UPLOADS CADA VEZ QUE ARRANQUE Y PARE */
 //		uploadFileService.deleteAll();
 		
-		//EJECUTAR LA PRIMERA VEZ CON ESTA LINEA PARA QUE NOS CREE LA CARPETA UPLOADS, COMENTARLA PARA SEGUNDOS ARRANQUES
-//		uploadFileService.init();
+		//EJECUTAR LA SEGUNDA VEZ CON ESTA LINEA PARA QUE NOS CREE LA CARPETA UPLOADS, COMENTARLA PARA EL PRIMER ARRANQUE
+		uploadFileService.init();
 		
 		
 		
